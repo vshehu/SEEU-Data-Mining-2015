b9529ae5ef3ee4c878e8d7f21a8097eaf1a29c00
vshehu <v.shehu@seeu.edu.mk>
2015-03-02 21:19:44 +0100

    Descriptive statistics implementation
    Design and implementation of an interface for descriptive statistics

---
+++ SEEU Data Mining/Statistics/IDescriptiveStatistics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SEEU_Data_Mining.Statistics
+{
+    /// <summary>
+    /// Interface qe implementon metodat e nevojshme per statistika deskriptive
+    /// Statistikat do te kalkulohen kundrejt nje objekti (varg, liste sipas deshires)
+    /// </summary>
+    public interface IDescriptiveStatistics
+    {
+        object Max();
+        object Min();
+        float Median();
+        float StandardDeviation();
+        float Mean();
+
+        Dictionary<float, int> Frequency(float[] data);
+        float Mode();
+    }
+}
