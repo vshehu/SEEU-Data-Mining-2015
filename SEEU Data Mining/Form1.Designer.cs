c39fbaddd071c4448fb39e677faa51f586145ff7
vshehu <v.shehu@seeu.edu.mk>
2015-03-16 12:06:52 +0100

    Implementim i spell checker ne Shqip
    Implementim i spell checker ne Shqip

--- SEEU Data Mining/Form1.Designer.cs
+++ SEEU Data Mining/Form1.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.txtFjala = new System.Windows.Forms.TextBox();
+            this.lstRekomandime = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(154, 39);
+            this.button1.Location = new System.Drawing.Point(78, 33);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -44,16 +45,25 @@
             // 
             // txtFjala
             // 
-            this.txtFjala.Location = new System.Drawing.Point(96, 13);
+            this.txtFjala.Location = new System.Drawing.Point(20, 7);
             this.txtFjala.Name = "txtFjala";
             this.txtFjala.Size = new System.Drawing.Size(133, 20);
             this.txtFjala.TabIndex = 1;
+            // 
+            // lstRekomandime
+            // 
+            this.lstRekomandime.FormattingEnabled = true;
+            this.lstRekomandime.Location = new System.Drawing.Point(12, 60);
+            this.lstRekomandime.Name = "lstRekomandime";
+            this.lstRekomandime.Size = new System.Drawing.Size(140, 108);
+            this.lstRekomandime.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(253, 78);
+            this.ClientSize = new System.Drawing.Size(165, 180);
+            this.Controls.Add(this.lstRekomandime);
             this.Controls.Add(this.txtFjala);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -67,6 +77,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox txtFjala;
+        private System.Windows.Forms.ListBox lstRekomandime;
     }
 }
 
--- SEEU Data Mining/Form1.cs
+++ SEEU Data Mining/Form1.cs
@@ -14,31 +14,44 @@ namespace SEEU_Data_Mining
 {
     public partial class Form1 : Form
     {
+        StreamReader sr = new StreamReader("c:\\data\\sq_AL.dic");
+        List<string> gjitheFjalet = new List<string>();
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            StreamReader sr = new StreamReader("c:\\data\\sq_AL.dic");
-
-            List<string> gjitheFjalet = new List<string>();
-
             while (sr.EndOfStream != true)
             {
                 gjitheFjalet.Add(sr.ReadLine().ToLower());
             }
+        }
 
-
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
             if (gjitheFjalet.Contains(txtFjala.Text.ToLower()))
             {
                 MessageBox.Show("Fjala eshte shkruar mire");
             }
             else
             {
-                 MessageBox.Show("Fjala eshte shkruar gabim");
+                Dictionary<string, int> rekomandimet = new Dictionary<string, int>();
+ 
+                LevensteinDistance l = new LevensteinDistance();
+                foreach (string fjala in gjitheFjalet)
+                {
+                    if (rekomandimet.ContainsKey(fjala) == false)
+                        rekomandimet.Add(fjala, l.Kalkulo(txtFjala.Text.ToLower(), fjala));
+                }
+
+                foreach (KeyValuePair<string, int> fjala in rekomandimet.OrderBy(x => x.Value).Take(5))
+                {
+                    lstRekomandime.Items.Add(fjala.Key +" - " + fjala.Value.ToString());
+                }
+
+
             }
+
+           
         }
     }
 }
